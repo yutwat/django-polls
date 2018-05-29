ae11bd8e53d044de38e15042d811bacb6d74899a
yutwat <yutwat@gmail.com>
2018-05-29 21:10:16 +0900

    third commit

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,8 +4,8 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
 
 	# ...
 	def __str__(self):
@@ -14,13 +14,13 @@ class Question(models.Model):
 	# ...
 	def was_published_recently(self):
 		return self.pud_date >= timezone.now() - datetime.timedelta(days=1)
-	
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-	# ...
-	def __str__(self):
-		return self.choice_text 
+    # ...
+    def __str__(self):
+    	return self.choice_text 
