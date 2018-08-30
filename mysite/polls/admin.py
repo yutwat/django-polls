32492c6e353dc7d5d6c092f83377d900b87c40ce
yutwat <yutwat@gmail.com>
2018-08-30 18:47:56 +0900

    css added and admin page has modified.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,12 +4,22 @@ from .models import Choice, Question
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-            (None,               {'fields': ['question_text']}),
-            ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-                ]
-    inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	fieldsets = [
+	(None,               {'fields': ['question_text']}),
+	('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
