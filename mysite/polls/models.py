607084af20f06ce5014561f6271ecd14b7330ecd
yutwat <yutwat@gmail.com>
2018-09-25 17:07:43 +0900

    detail.html was refined.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -22,6 +22,7 @@ class Question(models.Model):
 	pub_date = models.DateTimeField(
 		'date published'
 		)
+	
 
 	def __str__(self):
 		 return self.question_text
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -29,13 +29,14 @@ class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
 
+	form_class = AnswerForm()
+
 	def get_queryset(self):
 		"""
 		Excludes any questions that aren't published yet.
 		"""
 		return Question.objects.filter(pub_date__lte=timezone.now())
 
- 	
 
 class ResultsView(generic.DetailView):
 	model = Question
