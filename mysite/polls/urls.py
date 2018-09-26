2f13122eb7c32f575ea44edc7fe328b036a7ae50
yutwat <yutwat@gmail.com>
2018-09-26 16:21:40 +0900

    generic view class has been applied on urls.py.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import ListView
+from .models import Question
 
 from . import views
 
@@ -10,6 +12,11 @@ urlpatterns = [
     path('<int:question_id>/vote/', views.vote, name='vote'),
     path('<int:pk>/new/', views.post_new, name='post_new'),
     path('<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path(r'listview/', 
+    	ListView.as_view(
+    		queryset=Question.objects.all(), 
+    		template_name='polls/about.html'), 
+    	name='index_view'),
 ]
 
 # path('<int:question_id>/vote/', views.ResultsView.vote, name='vote'),
